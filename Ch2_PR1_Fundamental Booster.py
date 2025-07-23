b16917e47b09f893e40ec7bf6f2e2be05b87c8a8
Nidhi Patel <nidhipatel31701@gmail.com>
2025-07-23 18:25:32 +0530

    Add files via upload

---
+++ Ch2_PR1_Fundamental Booster.py
@@ -0,0 +1,25 @@
+import datetime
+
+
+print("Welcome to the Interactive Personal Data Collector!\n")
+
+name = input("Please Enter Your Name:")
+age = int(input("Please Enter Your Age:"))
+height = float(input("Please Enter Your Height in Meters:"))
+number = int(input("Please Enter Your Favourite Number:"))
+
+print("\n Thank Ypu! Here is the information we collected:\n")
+
+print("Name:", name, "  (Type:",type(name),"|" " Memory Address:", id(name),")")
+
+print("Age:", age, "   (Type:",type(age),"|" " Memory Address:", id(age),")")
+
+print("Height:",height, "   (Type:",type(height),"|" " Memory Address:", id(height),")")
+
+print("Favourite Number:",  number, "   (Type:",type(number),"|" " Memory Address:", id(number),")")
+
+year = 2025 - age 
+print("\n *** Your birth year is approximately:",year, "(based on your age of) ***\n")
+
+print("Thank You for using the Personal Data Collector.\n                        Goodbye!")
+
